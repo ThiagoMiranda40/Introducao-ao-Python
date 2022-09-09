2b993ab2740242f5d2ce379b10706d89b2e06d3a
ThiagoMiranda40 <contato@thiagomirandaoficial.com.br>
2022-09-09 04:11:20 -0300

    Fazendo comentários e organizando estrutura

---
+++ ProjetosFaculdade/MeuApp.py
@@ -0,0 +1,18 @@
+# DECLARAÇÃO DE VARIÁVEIS
+nome = 'Thiago Miranda'
+idade = 40
+profissão = 'Product Manager'
+print(nome, idade, profissão)
+
+
+
+# ENTRADA DE DADOS PELO USUÁRIO
+
+
+
+# PROCESSAMENTO DE DADOS
+
+
+
+
+# SAÍDA DE DADOS
